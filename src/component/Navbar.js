092cae2993fcf5f89aa02a7c37239d14dc408219
Animesh Raj <animesh080808@gmail.com>
2023-05-08 22:29:03 +0530

    feat : done for today
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (
-        <div>
-            <p>Navbar</p>
+        <div className="">
+            <ul id="nav">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/create-post">Create Post</Link>
+                </li>
+            </ul>
         </div>
     );
 }
